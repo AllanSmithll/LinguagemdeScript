8f63cd52859d753dc835dfb7a79667aded7c0c7e
AllaSmithll <allanammancio2004@gmail.com>
2022-09-04 21:32:12 -0300

    introdução-tiposdedados

---
+++ Introdução/tipos-dados.js
@@ -0,0 +1,7 @@
+const primitive1 = undefined
+const primitive2 = Null
+const primitive3 = Boolean
+const primitive4 = String
+const primitive5 = Number
+const primitive6 = Array
+const primitive7 = Object
