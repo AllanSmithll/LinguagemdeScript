2e5234f5b028c3c79987f8b2598a05212edf6688
AllaSmithll <allanammancio2004@gmail.com>
2022-09-11 15:22:55 -0300

    desafio 50

---
+++ Desafios/50-object-company.js
@@ -0,0 +1,28 @@
+const companies = [{
+    001: {
+        name: "Amazon",
+        founded: 1994,
+        industry: "E-Commerce, Cloud",
+        kind: "Internet Company"
+    },
+
+    002: {
+        name: "Facebook",
+        founded: 2004,
+        industry: "Social",
+        kind: "Internet Company"
+    },
+
+    003: {
+        name: "Alphabet Inc.",
+        founded: 2015,
+        industry: "Search, Cloud, Advertising",
+        kind: "Internet Company"
+    }
+}]
+
+function show(companies) {
+
+}
+
+show(companies)
