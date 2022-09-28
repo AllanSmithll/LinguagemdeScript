86a9079d4a02fdb74020ee985ec225902b7e5b27
AllaSmithll <allanammancio2004@gmail.com>
2022-09-27 22:49:37 -0300

    atualizando-pasta-função

--- 02 - Estruturas e Funções/Funções/parametrorest.js
+++ 02 - Estruturas e Funções/Funções/parametrorest.js
@@ -1,17 +1,17 @@
 // 27/09/2022
-// Parâmetro default
+// Parâmetro REST, o famoso "parâmetro infinito"
 
-function addition(...parametros) {
-    let summation = 0;
+function add(...parametros) {
+    let sum = 0;
     for (let v of parametros) {
-        summation += v                  
+        sum += v                  
         // AGORA SIM! Com essa estrutura de repetição, podemos saber a soma de todos os números que nós quisermos, pois colocamos "...parametros", que representa que os parâmetros podem ser infinitos, HEHEHE
     }
-    return summation
+    return sum
 }
 
  // Olha esse exemplo:
 
-console.log(addition(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) 
+console.log(add(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) 
 
 //O resultado deu corretíssimo, 550, no console de depuração Node.js
