c2cacc75c1f60faf010abeca46a3555fa196f4d7
AllaSmithll <allanammancio2004@gmail.com>
2022-10-02 17:04:21 -0300

    fim da iteração

---
+++ 04 - Arrays/deletando-valores.js
@@ -0,0 +1,8 @@
+// 30/09/2022
+// Deletar os valores de um array já criado
+
+numbers = [10, 20, 30, 40, 50, 60]
+
+delete numbers[0] // 
+delete numbers[4] // Nos dois casos é eliminado, os valores são eliminados a partir do Array, sem atualizá-lo
+console.log(numbers) // Acaba impimindo e deixando reticências no lugar do número
