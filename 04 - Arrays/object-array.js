48090a3d1ae17482a457ffa1e12356329f109981
AllaSmithll <allanammancio2004@gmail.com>
2022-10-03 22:39:14 -0300

    array finalizado

--- 04 - Arrays/object-array.js
+++ 04 - Arrays/object-array.js
@@ -21,6 +21,28 @@ console.log(number.reverse()) // Inverte os elementos do array, de trás para fr
 
 console.log(number.sort())  // Deixa a lista em ordem crescente da tabela ASCII
 
-console.log(number.slice(0, 2))  // Terira o elemento desses índices
+console.log(number.splice(0, 2))  // Retira os elementos desse intervalo, sendo o primeiro parâmetro o começo, o segundo vai até o índice declarado, menos 1, enquanto que o terceiro em diante serão os valores novos que serão colocados nos lugares de onde foi removido
 
 // Acessor: includes(), join(), slice()
+
+let acess = [2, 4, 6, 8]
+
+console.log(acess.includes(1)) // => Pergunta se o valor que está dentro dos parênteses se encontra na array
+
+console.log(acess.join("-")) // A lista acess será "ajuntada" por meio de hífens
+
+console.log(acess.slice(1, 2)) // Retorna os valores nesse intervalo, sem removê-los da array
+
+// Interation: map(), filter(), reduce(), every(), some()
+
+let interation = [2, 3, 5, 7, 9, 11];
+
+console.log(interation.map((value) => value * 2)) // Pega-se cada valor do array e multiplica por 2
+
+console.log(interation.filter((Value) => Value % 2 !== 0)) // Percorre-se o array em busca dos números que a divisão inteira por 2 seja diferente de 0. Se true, imprima na saída
+
+console.log(interation.reduce((addition, value) => addition + value, 0)) // Percorre-se o array, a partir da posição zero, e faz-se uma soma simples entre todos os números. Soma-se o número de índice 0, mais índice 1, mais índice 2 e assim por diante
+
+console.log(interation.every((value) => value % 2 == 0)) // Percorre-se o array em busca do seguinte: vai ser analisado absolutamente todos os números da lista; se todos os números obdecerem a condição imposta, o resultado no console será true. Caso não, false. Onde está a condição? Dentro dos parênteses de "every()"
+
+console.log(interation.some((value) => value % 2 == 0)) // Aqui já é um pouco diferente. Se pelo menos UM for true, imprime-se true no terminal. Caso contrário, false.
