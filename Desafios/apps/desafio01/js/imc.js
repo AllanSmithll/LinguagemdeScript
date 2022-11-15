02005379c506615bba8419b8c05b47ff8fc79c98
AllanSmithll <allanammancio2004@gmail.com>
2022-11-14 21:59:32 -0300

    Terminei exercício de calculadora IMC

--- Desafios/apps/desafio01/js/imc.js
+++ Desafios/apps/desafio01/js/imc.js
@@ -1,24 +1,171 @@
-function imc(peso, altura) {
-    let bmi = peso / (altura ** 2)
-    return bmi
-}
-
-function estado(bmi) {
-    let sex = ""
-    let result = ""
-    if (bmi < 19.1 && sex == "Feminino" || bmi < 20.7 && sex == "Masculino") {
-        result = "Abaixo do Peso"
-    } else if (bmi >= 19.1 && bmi <= 25.8 && sex == "Feminino" || bmi >= 20.7 && bmi <= 26.4 && sex == "Masculino") {
-        result = "Peso Normal"
-    } else if (bmi >= 25.8 && bmi <= 27.3 && sex == "Feminino" || bmi >= 26.4 && bmi <= 27.8 && sex == "Masculino") {
-        result = "Marginalmente Acima do Peso"
-    } else if (bmi >= 27.3 && bmi <= 32.3 && sex == "Feminino" || bmi >= 27.8 && bmi <= 31.1 && sex == "Masculino") {
-        result = "Acima do Peso Ideal"
-    } else if (bmi > 32.3 && sex == "Feminino" || bmi > 31.1 && sex == "Masculino") {
-        result = "Obeso"
+// IMC DATA
+const data = [
+    {
+      min: 0,
+      max: 18.4,
+      classification: "Menor que 18,5",
+      info: "Magreza",
+      obesity: "0",
+    },
+    {
+      min: 18.5,
+      max: 24.9,
+      classification: "Entre 18,5 e 24,9",
+      info: "Normal",
+      obesity: "0",
+    },
+    {
+      min: 25,
+      max: 29.9,
+      classification: "Entre 25,0 e 29,9",
+      info: "Sobrepeso",
+      obesity: "I",
+    },
+    {
+      min: 30,
+      max: 39.9,
+      classification: "Entre 30,0 e 39,9",
+      info: "Obesidade",
+      obesity: "II",
+    },
+    {
+      min: 40,
+      max: 99,
+      classification: "Maior que 40,0",
+      info: "Obesidade grave",
+      obesity: "III",
+    },
+  ];
+  
+  // Seleção de elementos
+  const imcTable = document.querySelector("#imc-table");
+  
+  const heightInput = document.querySelector("#height");
+  const weightInput = document.querySelector("#weight");
+  const calcBtn = document.querySelector("#calc-btn");
+  const clearBtn = document.querySelector("#clear-btn");
+  
+  const calcContainer = document.querySelector("#calc-container");
+  const resultContainer = document.querySelector("#result-container");
+  
+  const imcNumber = document.querySelector("#imc-number span");
+  const imcInfo = document.querySelector("#imc-info span");
+  
+  const backBtn = document.querySelector("#back-btn");
+  
+  // Funções
+  function createTable(data) {
+    data.forEach((item) => {
+      const div = document.createElement("div");
+      div.classList.add("table-data");
+  
+      const classification = document.createElement("p");
+      classification.innerText = item.classification;
+  
+      const info = document.createElement("p");
+      info.innerText = item.info;
+  
+      const obesity = document.createElement("p");
+      obesity.innerText = item.obesity;
+  
+      div.appendChild(classification);
+      div.appendChild(info);
+      div.appendChild(obesity);
+  
+      imcTable.appendChild(div);
+    });
+  }
+  
+  function validDigits(text) {
+    return text.replace(/[^0-9,]/g, "");
+  }
+  
+  function calcImc(height, weight) {
+    const imc = (weight / (height * height)).toFixed(1);
+    return imc;
+  }
+  
+  function cleanInputs() {
+    heightInput.value = "";
+    weightInput.value = "";
+    imcNumber.className = "";
+    imcInfo.className = "";
+  }
+  
+  function showOrHideResults() {
+    calcContainer.classList.toggle("hide");
+    resultContainer.classList.toggle("hide");
+  }
+  
+  // Init
+  createTable(data);
+  
+  // Eventos
+  [heightInput, weightInput].forEach((el) => {
+    el.addEventListener("input", (e) => {
+      const updatedValue = validDigits(e.target.value);
+  
+      e.target.value = updatedValue;
+    });
+  });
+  
+  calcBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+  
+    const weight = +weightInput.value.replace(",", ".");
+    const height = +heightInput.value.replace(",", ".");
+  
+    console.log(weight, height);
+  
+    if (!weight || !height) return;
+  
+    const imc = calcImc(height, weight);
+    let info;
+  
+    data.forEach((item) => {
+      if (imc >= item.min && imc <= item.max) {
+        info = item.info;
+      }
+    });
+  
+    if (!info) return;
+  
+    imcNumber.innerText = imc;
+    imcInfo.innerText = info;
+  
+    switch (info) {
+      case "Magreza":
+        imcNumber.classList.add("low");
+        imcInfo.classList.add("low");
+        break;
+      case "Normal":
+        imcNumber.classList.add("good");
+        imcInfo.classList.add("good");
+        break;
+      case "Sobrepeso":
+        imcNumber.classList.add("low");
+        imcInfo.classList.add("low");
+        break;
+      case "Obesidade":
+        imcNumber.classList.add("medium");
+        imcInfo.classList.add("medium");
+        break;
+      case "Obesidade grave":
+        imcNumber.classList.add("high");
+        imcInfo.classList.add("high");
+        break;
     }
-    return result
-}
-
-console.log(imc(60, 1.75))
-console.log(estado(imc(60, 1.75)))
+  
+    showOrHideResults();
+  });
+  
+  clearBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+  
+    cleanInputs();
+  });
+  
+  backBtn.addEventListener("click", (e) => {
+    cleanInputs();
+    showOrHideResults();
+  });
