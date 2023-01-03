911af1a8073b227297cee1f2e3c1165e5552d07c
AllanSmithll <allanammancio2004@gmail.com>
2023-01-02 22:24:23 -0300

    Requisição assíncrona com Promise

--- 09 - Criando Elementos Dinâmicos/scripts.js
+++ 09 - Criando Elementos Dinâmicos/scripts.js
@@ -1,7 +1,14 @@
 // 02/01/2023
 
 // const body = document.body
-// body.innerHTML = '<h2>Aula de Criação de Elementos dinâmicos</h2>' <-- // O body receberá esta frase ao lado esquerdo
+// body.innerHTML = '<h1>Aula de Criação de Elementos dinâmicos</h1>' // <-- O body receberá esta frase ao lado esquerdo
 
-const body2 = document.body
-body2.insertAdjacentElement("afterbegin", "<h1>Lorem Ipsum</h1>")
+// const body2 = document.body
+// body2.insertAdjacentElement("afterbegin", "<h1>Lorem Ipsum</h1>") // -> Logo depois do body é colocado este elemento H1
+
+const h1 = document.createElement("h1"); // -> Crio um elemento H1
+const text = document.createTextNode("Lorem ipsum"); // Crio um texto nó para o H1
+h1.appendChild(text); // -> H1 recebe o texto "text"
+
+const body = document.body; // Crio uma variável que funcionará como corpo do documento
+body.appendChild(h1); // -> Body receberá o elemento filho H1, que contém o texto nó
