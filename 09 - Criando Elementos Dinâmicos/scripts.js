969d14dc090a91252092f151ba83c7d5ac3f2c95
AllanSmithll <allanammancio2004@gmail.com>
2023-01-02 21:06:15 -0300

    Atualizações no repositório

---
+++ 09 - Criando Elementos Dinâmicos/scripts.js
@@ -0,0 +1,7 @@
+// 02/01/2023
+
+// const body = document.body
+// body.innerHTML = '<h2>Aula de Criação de Elementos dinâmicos</h2>' <-- // O body receberá esta frase ao lado esquerdo
+
+const body2 = document.body
+body2.insertAdjacentElement("afterbegin", "<h1>Lorem Ipsum</h1>")
